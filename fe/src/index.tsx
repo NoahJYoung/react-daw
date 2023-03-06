5827065e1e276285ce6b9f808a9ed5b62c493f17
ann <asog.jrti@gmail.com>
2023-03-06 01:58:33 +0100

    translate comment

--- fe/src/index.tsx
+++ fe/src/index.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import 'primereact/resources/primereact.min.css'; // hoja de estilo base de PrimeReact
-import 'primereact/resources/themes/saga-blue/theme.css'; // tema de PrimeReact que quieres utilizar
+import 'primereact/resources/primereact.min.css'; // base Primereact stylesheet
+import 'primereact/resources/themes/saga-blue/theme.css'; // Primereact theme
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
