9cc6f54c4a6df0840c0b21d2836a825b393f9cfb
ann <asog.jrti@gmail.com>
2023-03-06 01:57:19 +0100

    adds knobs for keyboard

--- fe/src/index.tsx
+++ fe/src/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import 'primereact/resources/primereact.min.css'; // hoja de estilo base de PrimeReact
+import 'primereact/resources/themes/saga-blue/theme.css'; // tema de PrimeReact que quieres utilizar
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
--- fe/src/pages/Workstation/components/InstrumentSection/components/Keyboard/components/ControlPanel/components/SynthControl.tsx
+++ fe/src/pages/Workstation/components/InstrumentSection/components/Keyboard/components/ControlPanel/components/SynthControl.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import { Slider, Typography } from 'antd';
+import { Typography } from 'antd';
+import { Knob } from 'primereact/knob';
 import styles from './SynthControl.module.scss';
 
 const { Text } = Typography
@@ -22,17 +23,22 @@ export const SynthControl: React.FC<SynthControlProps> = ({
     set,
 }) => {
         const [sliderValue, setSliderValue] = useState<number>(value);
+        const [knobColor, setKnobColor] = useState("#58ace8")
 
         const handleSliderChange = (value: number) => {
-            setSliderValue(value);
+            const parsedValue = Number(value.toFixed(2))
+            if (value < 0) setKnobColor("#db4646")
+            else setKnobColor("#58ace8")
+            setSliderValue(parsedValue);
         }
         return (
             <div className={styles.controlContainer}>
-                <Text>{ label }</Text>
-                <Slider
-                    onChange={handleSliderChange}
-                    onAfterChange={set}
+                <Text className={styles.knobLabel}>{ label }</Text>
+                <Knob
+                    valueColor={knobColor}
                     value={sliderValue}
+                    onChange={(e) => handleSliderChange(e.value)}
+                    size={60}
                     step={step}
                     min={min}
                     max={max}
